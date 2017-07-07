f9f9dd505ca9ef9acfaeb12747c9e4f768859b69
lenium <lena_ab@yahoo.com>
2017-07-06 20:29:34 -0600

    Corrected

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+setwd("C://Users//lena_/Documents//Coursera//Coursera Data Scientist/Getting and Cleaning Data//Week 4//Project")
 #---- Please download and unzip data here. This step could be scripted but preffered to omit those steps. 
 library(data.table)
 library(dplyr)
@@ -6,27 +7,31 @@ library(dplyr)
 activities<-read.table("./activity_labels.txt",col.names = c("activity_no","activity"))
 features<-read.table("./features.txt", col.names = c("feature_no","feature"))
 training<-read.table("./train/X_train.txt",col.names = features$feature)
-training$activity_no<-as.numeric(unlist((read.table("./train/Y_train.txt",col.names = "activity_no"))))
+training$activity_no<-as.numeric(unlist((read.table("./train/Y_train.txt" , col.names = "activity_no"))))
+training$subject_no<-as.numeric(unlist((read.table("./train/subject_train.txt" , col.names = "subject_no"))))
 row.names(training) = paste("training ",row.names(training))
 test<-read.table("./test/X_test.txt",col.names = features$feature)
-test$activity_no<-as.numeric(unlist((read.table("./test/Y_test.txt",col.names = "activity_no"))))
+test$activity_no<-as.numeric(unlist((read.table("./test/Y_test.txt" , col.names = "activity_no"))))
+test$subject_no<-as.numeric(unlist((read.table("./test/subject_test.txt", col.names = "subject_no"))))
 row.names(test) = paste("test ",row.names(test))
 data = rbind(test,training)
 #------ END Reading Data --------
+#-----START LABELING DATA -----
+label_func <- function(x) activities[x,2]
+data$activity_label <- sapply(data$activity_no, label_func)
+names(data)[1:561]=as.character(features[,2])
+
+#-----END LABELING DATA -------
 #----- START Extracting Data -----
-meanIndex = grep("[Mm]ean",names(data))
-stdIndex = grep("[Ss]td",names(data))
-extractedData = cbind(data[,meanIndex],data[,stdIndex],data$activity_no)
-colnames(extractedData) = c(colnames(data)[meanIndex],colnames(data)[stdIndex],"activity_no")
+Index = grep("[Mm]ean|[Ss]td",names(data))
+extractedData = cbind(data[,Index],data$activity_label,data$subject_no)
+colnames(extractedData) = c(colnames(data)[Index],"activity_label","subject_no")
 
 #------ END Extracting Data ------
 #----- START Creating NEW Data Set -----
-splittedData = split(extractedData,extractedData$activity_no)
-tidyData = unlist(lapply(splittedData,colMeans,na.rm = TRUE))
-names(tidyData) = paste("Average.",names(tidyData))
-for (i in 1:6) {
-        names(tidyData) = sub(as.character(i),activities$activity[i],names(tidyData))       
-}
-
+tidyData <- melt(extractedData, id=c("subject_no","activity_label"), measure.vars = names(extractedData)[1:86])
+tidyData <- group_by(tidyData, subject_no)
+tidyData <- group_by(tidyData, activity_label, add=TRUE)
+tidyData <- group_by(tidyData, variable, add=TRUE)
 #------ END Creating New Data Set ------
-write.table(tidyData, "TidyData.txt", col.name=FALSE)
+write.table(tidyData, "TidyData.txt", row.name=FALSE)
